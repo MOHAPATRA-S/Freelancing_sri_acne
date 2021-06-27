db1df3066dfcda8bb1d21e3f8675e9bbe06397c2
Swagatika Mohapatra <mohapatra.swagatika@thinksys.com>
2021-06-27 09:52:43 +0530

    base url changes and upload build on appstore

--- app/config.js
+++ app/config.js
@@ -1,6 +1,6 @@
 const config = {
   // apiGateway: "https://2f3ng4uvlf.execute-api.us-west-1.amazonaws.com/API",
-    apiGateway: "http://3.8.204.81:8010/api/v1",
+    apiGateway: "http://api.sriacne.com:8010/api/v1",
 };
 
 export default config;
