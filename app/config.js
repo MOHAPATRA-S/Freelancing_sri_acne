a6a69920bd8928a2981929d5c5e46d5763eefdc7
Swagatika Mohapatra <mohapatra.swagatika@thinksys.com>
2021-08-06 09:46:51 +0530

    base url vchange

--- app/config.js
+++ app/config.js
@@ -1,6 +1,6 @@
 const config = {
   // apiGateway: "https://2f3ng4uvlf.execute-api.us-west-1.amazonaws.com/API",
-    apiGateway: "http://api.sriacne.com:8000/api/v1",
+    apiGateway: "https://api.sriacne.com/api/v1",
 };
 
 export default config;
